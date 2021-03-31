3fa8a63d78f09c5597a6ab19fe19f3d9169fb873
Jay3000 <81451913+Jay3000@users.noreply.github.com>
2021-03-31 14:20:25 -0700

    health Pack

---
+++ Assets/HealthPack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPack : MonoBehaviour
+{
+
+    SpriteRenderer _rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rend = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void LateUpdate()
+    {
+        _rend.sortingOrder = Mathf.RoundToInt(transform.position.y) * -1;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
