d882b00ef4b97e005d26aea3ed3daf8ea6fa6853
Revolver1388 <45246957+Revolver1388@users.noreply.github.com>
2021-03-31 15:05:30 -0700

    added warning scene to game

---
+++ Assets/WarningScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class WarningScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private IEnumerator ChangeScene()
+    {
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadSceneAsync("Main_Menu");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
