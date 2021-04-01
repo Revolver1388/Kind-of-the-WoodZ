00f1bb5149965d414f269f1d6a8d0689ca5e09c6
Matt Riddett <matthew.riddett@gmail.com>
2021-03-31 20:18:59 -0700

    no mic mode

---
+++ Assets/NoMicController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoMicController : MonoBehaviour
+{
+    public GameObject OffMicButton;
+    public GameObject OnMicButton;
+
+    public void OnMic_Click()
+    {
+        OnMicButton.SetActive(false);
+        OffMicButton.SetActive(true);
+        PlayerPrefs.SetInt("MicMode", 1);
+        Debug.Log("Micmode is 1");
+    }
+
+    public void OffMic_Click()
+    {
+        OnMicButton.SetActive(true);
+        OffMicButton.SetActive(false);
+        PlayerPrefs.SetInt("MicMode", 0);
+
+        Debug.Log("Micmode is 0");
+
+
+    }
+
+}
